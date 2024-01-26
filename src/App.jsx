9cb9a8378bc05d3e45c94ce403bc734d221ab437
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-26 23:32:20 +0600

    load data and desply in ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BookFinder from "./components/BookFinder";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <BookFinder></BookFinder>
+    </>
+  );
+}
---
+++ src/components/BooksList.jsx
@@ -0,0 +1,11 @@
+import SingleBook from "./SingleBook";
+
+export default function BooksList({ books }) {
+  return (
+    <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-fr">
+      {books.books.map((book) => (
+        <SingleBook key={book.id} book={book}></SingleBook>
+      ))}
+    </div>
+  );
+}
